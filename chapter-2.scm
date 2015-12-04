90f23414ba30a17a4dd280818853727a4f97e464
王宏伟 <wanghongwei@51zhangdan.com.cn>
2015-12-04 19:25:55 +0800

    3.36

--- chapter-2.scm
+++ chapter-2.scm
@@ -577,9 +577,9 @@
                       (cdr sequence)))))
 
 
-(define (map p sequence)
-  (accumulate (lambda (x y) (append (cons (p x) nil) y)) 
-              nil sequence))
+; (define (map p sequence)
+;   (accumulate (lambda (x y) (append (cons (p x) nil) y)) 
+;               nil sequence))
 
 ; (display (map square (list 1 2 3)))
 
@@ -643,6 +643,29 @@
 ; (display (accumulate-n + 0 sq))
 
 
+
+; 2.37
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map (lambda (row) (dot-product row v)) m))
+
+(define (transpose mat)
+  (accumulate-n cons nil mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (row) (matrix-*-vector cols row)) 
+         m)))
+
+; (define m (list (list 1 2 3 4) (list 4 5 6 6) (list 6 7 8 9)))
+; (define v (list 1 1 1 1))
+; (display (matrix-*-vector m v))
+; (newline)
+; (display (transpose m))
+; (newline)
+; (display (matrix-*-matrix m (list (list 1 1 1) (list 1 1 1) (list 1 1 1) (list 1 1 1))))
 
 
 

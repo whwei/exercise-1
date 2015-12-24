305e2b486e2ab411d9d708d025675f7a900acc5f
王宏伟 <wanghongwei@51zhangdan.com.cn>
2015-12-24 18:46:05 +0800

    to 2.76

--- chapter-2.scm
+++ chapter-2.scm
@@ -1193,6 +1193,32 @@
 
 
 ; 2.75
+(define (make-from-mag-ang r a)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) (* r (cos a)))
+          ((eq? op 'imag-part) (* r (sin a)))
+          ((eq? op 'magnitude) r)
+          ((eq? op 'angle) a)
+          (else
+           (error "Unknown op: 
+            MAKE-FROM-MAG-ANG" op))))
+  dispatch)
+
+
+
+; 2.76
+; new operations must often be added: message-passing-style
+; new types must often be added: data-directed-style
+
+
+
+
+
+
+
+
+
+
 
 
 
